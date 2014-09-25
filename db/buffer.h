854255e9f6cfbc715c3598bc5d07c4533b07a3a6
Donovan Hide <donovanhide@gmail.com>
2014-09-25 18:00:12 +0100

    More buffer tests

--- db/buffer.h
+++ db/buffer.h
@@ -9,6 +9,7 @@
 
 namespace keyvadb {
 
+// Buffer is thread-safe
 template <uint32_t BITS, uint32_t DEGREE>
 class Buffer {
   typedef Key<BITS> key_t;
@@ -18,17 +19,27 @@ class Buffer {
   std::mutex lock_;
 
  public:
-  void Add(const key_t& key, const uint64_t value) {
+  size_t Add(const key_t& key, const uint64_t value) {
     std::lock_guard<std::mutex> lock(lock_);
-    map_.emplace(std::make_pair(key, value));
+    map_.emplace(key, value);
+    return map_.size();
+  }
+
+  bool Remove(KeyValue<BITS> const& key) {
+    std::lock_guard<std::mutex> lock(lock_);
+    return map_.erase(key.key) > 0;
+  }
+
+  bool Swap(KeyValue<BITS> const& a, KeyValue<BITS> const& b) {
+    std::lock_guard<std::mutex> lock(lock_);
+    map_.emplace(b.key, b.value);
+    return map_.erase(a.key) > 0;
   }
 
   size_t Size() {
     std::lock_guard<std::mutex> lock(lock_);
     return map_.size();
   }
-
-  void Swap(Node<BITS, DEGREE>& node) {}
 };
 
 }  // namespace keyvadb
--- tests/buffer_unittest.cc
+++ tests/buffer_unittest.cc
@@ -8,6 +8,23 @@ TEST(BufferTests, General) {
   Buffer<256, 84> buffer;
   ASSERT_EQ(0, buffer.Size());
   auto keys = RandomKeys<256>(1000);
-  for (auto key : keys) buffer.Add(key, 0);
+  // Adds
+  for (auto const& key : keys) buffer.Add(key, 0);
   ASSERT_EQ(keys.size(), buffer.Size());
+  // Removes
+  for (auto const& key : keys) {
+    auto kv = KeyValue<256>{key, 0};
+    ASSERT_TRUE(buffer.Remove(kv));
+    ASSERT_FALSE(buffer.Remove(kv));
+  };
+  ASSERT_EQ(0, buffer.Size());
+  // Swaps
+  for (size_t i = 0; i < 500; i++) ASSERT_EQ(i + 1, buffer.Add(keys.at(i), i));
+  ASSERT_EQ(500, buffer.Size());
+  for (size_t i = 0; i < 500; i++) {
+    auto existing = KeyValue<256>{keys.at(i), i};
+    auto add = KeyValue<256>{keys.at(i + 500), i + 500};
+    ASSERT_TRUE(buffer.Swap(existing, add));
+  }
+  ASSERT_EQ(500, buffer.Size());
 }
