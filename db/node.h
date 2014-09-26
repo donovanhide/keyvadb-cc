dde9d9765395e75623935aeb7919a6b6177ec138
Donovan Hide <donovanhide@gmail.com>
2014-09-26 12:47:32 +0100

    Clear up const naming

--- db/node.h
+++ db/node.h
@@ -8,7 +8,11 @@
 
 namespace keyvadb {
 
-static const uint64_t SyntheticChild = std::numeric_limits<uint64_t>::max();
+static const uint64_t EmptyKey = 0;
+
+static const uint64_t SyntheticValue = std::numeric_limits<uint64_t>::max();
+static const uint64_t EmptyValue = 0;
+
 static const uint64_t EmptyChild = 0;
 
 template <uint32_t BITS>
@@ -41,19 +45,21 @@ class Node {
   Node(uint64_t id, Key<BITS> const& first, Key<BITS> const& last)
       : id_(id), first_(first), last_(last) {
     if (first >= last) throw std::domain_error("first must be lower than last");
-    static auto zero = KeyValue<BITS>{0, 0};
+    static auto zero = KeyValue<BITS>{EmptyKey, EmptyValue};
     std::fill(keys_.begin(), keys_.end(), zero);
     std::fill(children_.begin(), children_.end(), EmptyChild);
   }
 
-  void AddSyntheticKeys() {
-    auto stride = Stride(first_, last_, DEGREE);
+  void AddSyntheticKeyValues() {
+    auto const stride = Stride();
     auto cursor = first_ + stride;
     for (auto& key : keys_) {
-      key = KeyValue<BITS>{cursor, SyntheticChild};
+      key = KeyValue<BITS>{cursor, SyntheticValue};
       cursor += stride;
     }
   }
+
+  constexpr KeyValue<BITS> GetKeyValue(size_t const i) { return keys_[i]; }
 
   constexpr bool IsSane() {
     if (first_ >= last_) return false;
@@ -73,10 +79,12 @@ class Node {
   }
   constexpr size_t KeyCount() { return keys_.size(); }
   constexpr size_t ChildCount() { return children_.size(); }
+  constexpr Key<BITS> Distance() { return keyvadb::Distance(first_, last_); }
+  constexpr Key<BITS> Stride() {
+    return keyvadb::Stride(first_, last_, ChildCount());
+  }
 
   friend std::ostream& operator<<(std::ostream& stream, const Node& node) {
-    auto stride = ToHex(Stride(node.first_, node.last_, node.ChildCount()));
-    auto distance = ToHex(Distance(node.first_, node.last_));
     stream << "Id:\t\t" << node.id_ << std::endl;
     stream << "Keys:\t\t" << node.KeyCount() - node.EmptyKeyCount()
            << std::endl;
@@ -84,14 +92,19 @@ class Node {
            << std::endl;
     stream << "First:\t\t" << ToHex(node.first_) << std::endl;
     stream << "Last:\t\t" << ToHex(node.last_) << std::endl;
-    stream << "Stride:\t\t" << stride << std::endl;
-    stream << "Distance:\t" << distance << std::endl;
+    stream << "Stride:\t\t" << ToHex(node.Stride()) << std::endl;
+    stream << "Distance:\t" << ToHex(node.Distance()) << std::endl;
     stream << "--------" << std::endl;
     for (size_t i = 0; i < node.KeyCount(); i++) {
       stream << std::setfill('0') << std::setw(3) << i << " ";
       stream << ToHex(node.keys_[i].key) << " ";
-      stream << node.keys_[i].value << " " << node.children_[i];
-      if (i == node.KeyCount() - 1) stream << " " << node.children_[i + 1];
+      if (node.keys_[i].value == SyntheticValue) {
+        stream << "Synthetic"
+               << " ";
+      } else {
+        stream << node.keys_[i].value << " ";
+      }
+      stream << node.children_[i] << " " << node.children_[i + 1];
       stream << std::endl;
     }
     stream << "--------" << std::endl;
--- tests/node_unittest.cc
+++ tests/node_unittest.cc
@@ -4,7 +4,7 @@
 
 using namespace keyvadb;
 
-TEST(NodeTests, General) {
+TEST(NodeTests, BigNode) {
   Key<256> first;
   Key<256> last;
   FromHex(first, h0);
@@ -16,6 +16,22 @@ TEST(NodeTests, General) {
   ASSERT_EQ(84, node.EmptyChildCount());
   ASSERT_EQ(83, node.KeyCount());
   ASSERT_EQ(83, node.EmptyKeyCount());
-  node.AddSyntheticKeys();
+  node.AddSyntheticKeyValues();
   ASSERT_TRUE(node.IsSane());
+}
+
+TEST(NodeTests, SmallNode) {
+  Key<256> first;
+  Key<256> last;
+  FromHex(first, h0);
+  FromHex(last, h2);
+  Node<256, 16> node(0, first, last);
+  ASSERT_TRUE(node.IsSane());
+  node.AddSyntheticKeyValues();
+  ASSERT_TRUE(node.IsSane());
+  Key<256> middle;
+  FromHex(middle, h9);
+  ASSERT_EQ(middle, node.GetKeyValue(7).key);
+  ASSERT_EQ(SyntheticValue, node.GetKeyValue(7).value);
+  std::cout << node;
 }
