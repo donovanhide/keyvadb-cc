727648de4b7d54cb32c6872e77d488be3a66c3e1
Donovan Hide <donny@sur.ly>
2014-10-11 20:03:23 +0100

    Templated key tests

--- tests/key_unittest.h
+++ tests/key_unittest.h
@@ -1,25 +1,52 @@
+#include <string>
 #include "tests/common.h"
 #include "db/key.h"
 
 using namespace keyvadb;
 
-TEST(KeyTests, General) {
-  // Constructors and strings
-  Key<256> zero, first, last, ones, twos, threes, two, tooBig;
-  FromHex(zero, h0);
-  FromHex(first, h1);
-  FromHex(last, h2);
-  FromHex(ones, h3);
-  FromHex(twos, h4);
-  FromHex(threes, h6);
-  FromHex(two, h7);
-  ASSERT_EQ(h0, ToHex(zero));
-  ASSERT_EQ(h1, ToHex(first));
-  ASSERT_EQ(h2, ToHex(last));
+template <std::uint32_t BITS>
+struct KeyTraits {
+  enum { Bits = BITS };
+};
+
+template <typename T>
+class KeyTest : public ::testing::Test {
+  using key_type = Key<T::Bits>;
+
+ private:
+  T traits_;
+
+ protected:
+  key_type GetFromHex(std::string const& s) {
+    key_type key;
+    FromHex<T::Bits>(key, s);
+    return key;
+  }
+  key_type GetFromBytes(std::string const& s) { return FromBytes<T::Bits>(s); }
+  key_type GetMax() { return Max<T::Bits>(); }
+  key_type GetMin() { return Min<T::Bits>(); }
+  key_type MakeKey(std::uint32_t n) { return key_type(n); }
+  key_type MakeKeyFromHex(const char c) {
+    return GetFromHex(std::string(T::Bits / 4, c));
+  }
+  key_type TooBig() { return GetFromHex(std::string(T::Bits, 'F')); }
+};
+
+typedef ::testing::Types<KeyTraits<1024>, KeyTraits<256>, KeyTraits<32>,
+                         KeyTraits<8>> KeyTypes;
+TYPED_TEST_CASE(KeyTest, KeyTypes);
+
+TYPED_TEST(KeyTest, General) {
+  auto zero = this->MakeKey(0);
+  auto two = this->MakeKey(2);
+  auto first = this->MakeKey(1);
+  auto last = this->MakeKeyFromHex('F');
+  auto ones = this->MakeKeyFromHex('1');
+  auto twos = this->MakeKeyFromHex('2');
+  auto threes = this->MakeKeyFromHex('3');
   // Min/Max
-  ASSERT_EQ(zero, Min<256>());
-  ASSERT_EQ(first, Min<256>() + 1);
-  ASSERT_EQ(last, Max<256>());
+  ASSERT_EQ(zero, this->GetMin());
+  ASSERT_EQ(last, this->GetMax());
   // Comparisons
   ASSERT_TRUE(zero.is_zero());
   ASSERT_TRUE(first < last);
@@ -30,7 +57,7 @@ TEST(KeyTests, General) {
   // Exceptions
   ASSERT_THROW(last + first, std::overflow_error);
   ASSERT_THROW(first - last, std::range_error);
-  ASSERT_THROW(FromHex(tooBig, h8), std::overflow_error);
+  ASSERT_THROW(this->TooBig(), std::overflow_error);
   // Distances
   ASSERT_EQ(ones, Distance(threes, twos));
   ASSERT_EQ(ones, Distance(twos, threes));
@@ -39,7 +66,7 @@ TEST(KeyTests, General) {
   ASSERT_EQ(ones, stride);
   // Nearest
   uint32_t nearest;
-  Key<256> distance;
+  auto distance = this->MakeKey(0);
   NearestStride(zero, stride, ones, distance, nearest);
   ASSERT_EQ(zero, distance);
   ASSERT_EQ(0UL, nearest);
@@ -51,9 +78,9 @@ TEST(KeyTests, General) {
   ASSERT_EQ(0UL, nearest);
   // From/To bytes
   auto f = ToBytes(first);
-  auto f2 = FromBytes<256>(f);
+  auto f2 = this->GetFromBytes(f);
   ASSERT_EQ(first, f2);
   auto l = ToBytes(last);
-  auto l2 = FromBytes<256>(l);
+  auto l2 = this->GetFromBytes(l);
   ASSERT_EQ(last, l2);
 }
