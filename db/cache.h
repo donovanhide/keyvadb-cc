a0fb0138c84a8fbb9a20e18a3f47854a446fc68f
Donovan Hide <donovanhide@gmail.com>
2014-11-11 16:29:10 +0000

    Cache unittests

--- db/cache.h
+++ db/cache.h
@@ -8,103 +8,121 @@
 #include <mutex>
 #include "db/key.h"
 
-namespace keyvadb {
-
+namespace keyvadb
+{
 template <std::uint32_t BITS>
-class NodeCache {
-  using util = detail::KeyUtil<BITS>;
-  using key_type = typename util::key_type;
-  using node_type = Node<BITS>;
-  using node_ptr = std::shared_ptr<node_type>;
-  using key_level_pair = std::pair<std::uint32_t, key_type>;
-  struct compare_kl {
-    bool operator()(key_level_pair const& lhs,
-                    key_level_pair const& rhs) const {
-      if (lhs.first == rhs.first)
-        return lhs.second < rhs.second;
-      return lhs.first > rhs.first;
-    }
-  };
-  using index_type =
-      boost::bimaps::bimap<boost::bimaps::set_of<key_level_pair, compare_kl>,
-                           boost::bimaps::list_of<node_ptr>>;
-  using index_value = typename index_type::value_type;
-
- private:
-  std::uint64_t maxSize_ = 0;
-  std::uint64_t hits_ = 0;
-  std::uint64_t misses_ = 0;
-  std::uint64_t inserts_ = 0;
-  std::uint64_t updates_ = 0;
-  index_type nodes_;
-  std::mutex lock_;
-
- public:
-  void SetMaxSize(std::uint64_t maxSize) {
-    std::lock_guard<std::mutex> lock(lock_);
-    maxSize_ = maxSize;
-  }
-
-  void Reset() {
-    std::lock_guard<std::mutex> lock(lock_);
-    hits_ = 0;
-    misses_ = 0;
-    inserts_ = 0;
-    updates_ = 0;
-    nodes_.clear();
-  }
-
-  void Add(node_ptr const& node) {
-    if (maxSize_ == 0)
-      return;
-    std::lock_guard<std::mutex> lock(lock_);
-    auto it = nodes_.left.find(key_level_pair{node->Level(), node->First()});
-    if (it != nodes_.left.end()) {
-      updates_++;
-      it->second = node;
-      nodes_.right.relocate(nodes_.right.end(), nodes_.project_right(it));
-    } else {
-      inserts_++;
-      nodes_.insert(index_value({node->Level(), node->First()}, node));
-      if (nodes_.size() > maxSize_)
-        nodes_.right.erase(nodes_.right.begin());
-    }
-  }
-
-  node_ptr Get(key_type const& key) {
+class NodeCache
+{
     using util = detail::KeyUtil<BITS>;
-    std::lock_guard<std::mutex> lock(lock_);
-    if (maxSize_ == 0 || nodes_.size() == 0)
-      return node_ptr();
-    auto level = nodes_.left.begin()->first.first + 1;
-    for (; level > 0; level--) {
-      auto it = nodes_.left.upper_bound(key_level_pair{level, key});
-      if (it != nodes_.left.begin())
-        it--;
-      if (it->second->Level() > level)
-        break;
-      if (it->second->First() < key && it->second->Last() > key) {
-        hits_++;
-        nodes_.right.relocate(nodes_.right.end(), nodes_.project_right(it));
-        return it->second;
-      }
-    }
-    misses_++;
-    // std::cout << "Miss:" << util::ToHex(key) << std::endl;
-    return node_ptr();
-  }
+    using key_type = typename util::key_type;
+    using node_type = Node<BITS>;
+    using node_ptr = std::shared_ptr<node_type>;
+    using key_level_pair = std::pair<std::uint32_t, key_type>;
+    struct compare_kl
+    {
+        bool operator()(key_level_pair const& lhs,
+                        key_level_pair const& rhs) const
+        {
+            if (lhs.first == rhs.first)
+                return lhs.second < rhs.second;
+            return lhs.first > rhs.first;
+        }
+    };
+    using index_type =
+        boost::bimaps::bimap<boost::bimaps::set_of<key_level_pair, compare_kl>,
+                             boost::bimaps::list_of<node_ptr>>;
+    using index_value = typename index_type::value_type;
 
-  friend std::ostream& operator<<(std::ostream& stream, NodeCache& cache) {
-    std::lock_guard<std::mutex> lock(cache.lock_);
-    stream << "Size: " << cache.nodes_.size() << "/" << cache.maxSize_
-           << " Hits: " << cache.hits_ << " Misses: " << cache.misses_
-           << " Inserts:" << cache.inserts_ << " Updates: " << cache.updates_
-           << std::endl;
-    // for (auto it = std::cbegin(cache.nodes_); it != std::cend(cache.nodes_);
-    //      ++it)
-    //   stream << it->left.first << ":" << util::ToHex(it->left.second)
-    //          << std::endl;
-    return stream;
-  }
+   private:
+    std::uint64_t maxSize_ = 0;
+    std::uint64_t hits_ = 0;
+    std::uint64_t misses_ = 0;
+    std::uint64_t inserts_ = 0;
+    std::uint64_t updates_ = 0;
+    index_type nodes_;
+    std::mutex lock_;
+
+   public:
+    void SetMaxSize(std::uint64_t maxSize)
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        maxSize_ = maxSize;
+    }
+
+    void Reset()
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        hits_ = 0;
+        misses_ = 0;
+        inserts_ = 0;
+        updates_ = 0;
+        nodes_.clear();
+    }
+
+    void Add(node_ptr const& node)
+    {
+        if (maxSize_ == 0)
+            return;
+        std::lock_guard<std::mutex> lock(lock_);
+        auto it =
+            nodes_.left.find(key_level_pair{node->Level(), node->First()});
+        if (it != nodes_.left.end())
+        {
+            updates_++;
+            it->second = node;
+            nodes_.right.relocate(nodes_.right.end(), nodes_.project_right(it));
+        }
+        else
+        {
+            inserts_++;
+            nodes_.insert(index_value({node->Level(), node->First()}, node));
+            if (nodes_.size() > maxSize_)
+                nodes_.right.erase(nodes_.right.begin());
+        }
+    }
+
+    // Get node lowest in the tree by checking deepest nodes in the cache first.
+    // Key 0000...0000 will always return a null node_ptr.
+    node_ptr Get(key_type const& key)
+    {
+        using util = detail::KeyUtil<BITS>;
+        std::lock_guard<std::mutex> lock(lock_);
+        if (maxSize_ == 0 || nodes_.size() == 0)
+            return node_ptr();
+        auto level = nodes_.left.begin()->first.first + 1;
+        for (; level > 0; level--)
+        {
+            auto it = nodes_.left.upper_bound(key_level_pair{level, key});
+            if (it != nodes_.left.begin())
+                it--;
+            if (it->second->Level() > level)
+                break;
+            if (it->second->First() < key && it->second->Last() > key)
+            {
+                hits_++;
+                nodes_.right.relocate(nodes_.right.end(),
+                                      nodes_.project_right(it));
+                return it->second;
+            }
+        }
+        misses_++;
+        // std::cout << "Miss:" << util::ToHex(key) << std::endl;
+        return node_ptr();
+    }
+
+    friend std::ostream& operator<<(std::ostream& stream, NodeCache& cache)
+    {
+        std::lock_guard<std::mutex> lock(cache.lock_);
+        stream << "Size: " << cache.nodes_.size() << "/" << cache.maxSize_
+               << " Hits: " << cache.hits_ << " Misses: " << cache.misses_
+               << " Inserts:" << cache.inserts_
+               << " Updates: " << cache.updates_ << std::endl;
+        // for (auto it = std::cbegin(cache.nodes_); it !=
+        // std::cend(cache.nodes_);
+        //      ++it)
+        //   stream << it->left.first << ":" << util::ToHex(it->left.second)
+        //          << std::endl;
+        return stream;
+    }
 };
 }  // namespace keyvadb
--- tests/store_unittest.h
+++ tests/store_unittest.h
@@ -6,48 +6,68 @@
 
 using namespace keyvadb;
 
-TYPED_TEST(StoreTest, SetAndGetKeys) {
-  auto first = this->MakeKey(1);
-  auto last = this->FromHex('F');
-  auto root = this->keys_->New(0, first, last);
-  ASSERT_EQ(0UL, root->Id());
-  ASSERT_EQ(first, root->First());
-  ASSERT_EQ(last, root->Last());
-  root->AddSyntheticKeyValues();
-  ASSERT_TRUE(root->IsSane());
-  std::error_condition err;
-  auto node = this->EmptyNode();
-  std::tie(node, err) = this->keys_->Get(root->Id());
-  ASSERT_EQ(db_error::key_not_found, err.value());
-  ASSERT_EQ(nullptr, node);
-  ASSERT_FALSE(this->keys_->Set(root));
-  std::tie(node, err) = this->keys_->Get(root->Id());
-  ASSERT_FALSE(err);
-  ASSERT_EQ(root->Last(), node->Last());
-  ASSERT_TRUE(node->IsSane());
+TYPED_TEST(StoreTest, SetAndGetKeys)
+{
+    auto first = this->MakeKey(0);
+    auto last = this->FromHex('F');
+    auto root = this->keys_->New(0, first, last);
+    ASSERT_EQ(0UL, root->Id());
+    ASSERT_EQ(first, root->First());
+    ASSERT_EQ(last, root->Last());
+    root->AddSyntheticKeyValues();
+    ASSERT_TRUE(root->IsSane());
+    std::error_condition err;
+    auto node = this->EmptyNode();
+    std::tie(node, err) = this->keys_->Get(root->Id());
+    ASSERT_EQ(db_error::key_not_found, err.value());
+    ASSERT_EQ(nullptr, node);
+    ASSERT_FALSE(this->keys_->Set(root));
+    std::tie(node, err) = this->keys_->Get(root->Id());
+    ASSERT_FALSE(err);
+    ASSERT_EQ(root->Last(), node->Last());
+    ASSERT_TRUE(node->IsSane());
 }
 
-TYPED_TEST(StoreTest, SetAndGetValues) {
-  auto key1 = this->HexString('1');
-  auto key2 = this->HexString('2');
-  std::string value1("First Value");
-  std::string value2("Second Value");
-  auto kv1 = this->EmptyKeyValue();
-  auto kv2 = this->EmptyKeyValue();
-  ASSERT_FALSE(this->values_->Set(key1, value1, kv1));
-  ASSERT_FALSE(this->values_->Set(key2, value2, kv2));
-  std::string got1, got2;
-  ASSERT_FALSE(this->values_->Get(kv1.value, &got1));
-  ASSERT_FALSE(this->values_->Get(kv2.value, &got2));
-  ASSERT_EQ(value1, got1);
-  ASSERT_EQ(value2, got2);
+TYPED_TEST(StoreTest, SetAndGetValues)
+{
+    auto key1 = this->HexString('1');
+    auto key2 = this->HexString('2');
+    std::string value1("First Value");
+    std::string value2("Second Value");
+    auto kv1 = this->EmptyKeyValue();
+    auto kv2 = this->EmptyKeyValue();
+    ASSERT_FALSE(this->values_->Set(key1, value1, kv1));
+    ASSERT_FALSE(this->values_->Set(key2, value2, kv2));
+    std::string got1, got2;
+    ASSERT_FALSE(this->values_->Get(kv1.value, &got1));
+    ASSERT_FALSE(this->values_->Get(kv2.value, &got2));
+    ASSERT_EQ(value1, got1);
+    ASSERT_EQ(value2, got2);
 }
 
-TYPED_TEST(StoreTest, Cache) {
-  this->cache_.SetMaxSize(2);
-  auto key1 = this->FromHex('1');
-  auto key2 = this->FromHex('2');
-  auto key3 = this->FromHex('3');
-  auto key4 = this->FromHex('4');
-  this->cache_.Add(this->keys_->New(0, key1, key4));
+TYPED_TEST(StoreTest, Cache)
+{
+    this->cache_.SetMaxSize(2);
+    auto first = this->MakeKey(0);
+    auto last = this->FromHex('F');
+    auto key1 = this->FromHex('1');
+    auto key2 = this->FromHex('2');
+    auto key3 = this->FromHex('3');
+    auto key4 = this->FromHex('4');
+    auto key5 = this->FromHex('5');
+    auto root = this->keys_->New(0, first, last);
+    auto firstChild = this->keys_->New(1, key1, key5);
+    auto secondChild = this->keys_->New(2, key2, key4);
+    // The key 0000... can never be found in the cache
+    ASSERT_FALSE(this->cache_.Get(first));
+    this->cache_.Add(root);
+    ASSERT_FALSE(this->cache_.Get(first));
+    // The key 0000...0001 is the first key that can possibly be found
+    ASSERT_EQ(this->cache_.Get(first+1), root);
+    this->cache_.Add(firstChild);
+    ASSERT_EQ(this->cache_.Get(key1+1), firstChild);
+    this->cache_.Add(secondChild);
+    ASSERT_EQ(this->cache_.Get(key2+1), secondChild);
+    // root now evicted
+    ASSERT_FALSE(this->cache_.Get(first + 1));
 }
